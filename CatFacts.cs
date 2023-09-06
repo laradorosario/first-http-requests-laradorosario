7fac98d975892eb90a855c5744d3b6c53fc58fdc
Jason Cox <coxj@vt.edu>
2023-09-06 17:04:44 -0400

    Update CatFacts.cs

--- CatFacts.cs
+++ CatFacts.cs
@@ -5,3 +5,5 @@ public record struct FactStatus(bool verified, int sentCount);
 public record struct CatFact(FactStatus status, string _id, string user,
     string text, string source, DateTime updatedAt, string type, 
     DateTime createdAt, bool deleted, bool used);
+    
+public record struct CatFactShort(string text, string source, string type, DateTime createdAt);
