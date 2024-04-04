5d7410400603eda1dbb3fd0f84d6fec2ef667af4
laradorosario <158072496+laradorosario@users.noreply.github.com>
2024-04-04 11:32:55 -0400

    hi

---
+++ WinsorApps.cs
@@ -0,0 +1,20 @@
+namespace CS_First_HTTP_Client;
+
+{
+    /// <summary>
+    /// Object (Login) to be used with Winsor Apps API
+    /// </summary>
+    /// <param name="email"></param>
+    /// <param name="password"></param>
+    public readonly record struct Login(string email, string password);
+    /// <summary>
+    /// Error response from the Winsor Apps API
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="error"></param>
+    public readonly record struct ErrorResponse(string type, string error);
+    
+    public readonly record struct AuthResponse(string userId, string jwt, DateTime expires, string refreshToken);
+    
+    public readonly record struct StudentProfile(string id, string firstName, string lastName, string email, bool hasLogin)
+}
